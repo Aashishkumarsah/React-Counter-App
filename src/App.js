7c6e7f6af6855a99af4207c8cc6d23b32574327e
Aashish Kumar Sah <ashishsah637@gmail.com>
2025-07-12 13:20:17 +0545

    Final Props Counter Button Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const AddButton = (props) => {
+
+  const onIncrementClick = () => {
+    props.setCount (props.count + 1);
+  }
+
+  return (
+    <button onClick={onIncrementClick}>Increment</button>
+  )
+}
+
+const DecrementButton = (props) => {
+
+   const onDecrementClick = () => {
+    props.setCount (props.count - 1);
+  }
+
+  return (
+    <button onClick={ onDecrementClick }>Decrement</button>
+  )
+}
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <AddButton count={count} setCount={setCount}/>
+      <DecrementButton count={count} setCount={setCount} />
     </div>
   );
 }
